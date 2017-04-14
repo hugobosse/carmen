8e3cdef75ebcad0e7078fdfebd69d51b9385950a
hugo bosse <monsieurbosse@gmail.com>
2017-04-14 09:12:41 +0200

    new pages with routes and controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :concept ]
 
   def home
+  end
+
+  def about
+  end
+
+  def concept
   end
 end
