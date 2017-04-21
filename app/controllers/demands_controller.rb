d13271b560ce4b790c6c8ad8f670a39efc284dec
hugo bosse <monsieurbosse@gmail.com>
2017-04-21 14:48:57 +0200

    simple form working with 1 creteria

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -1,2 +1,20 @@
 class DemandsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:create]
+
+  def create
+    @demand = Demand.new(demand_params)
+    @demand.save
+    if @demand.save
+      redirect_to root_path, notice: "Correct"
+    else
+      redirect_to root_path, notice: "Wrong"
+    end
+  end
+
+  private
+
+  def demand_params
+    params.require(:demand).permit(:people)
+  end
+
 end
--- app/models/demand.rb
+++ app/models/demand.rb
@@ -1,2 +1,10 @@
 class Demand < ApplicationRecord
+  validates :people, presence: true
+  #validates :address, presence: true
+  #validates :date, presence: true
+  #validates :budget, presence: true
+  #validates :phone, presence: true
+  #validates :name, presence: true
+  #validates :cuisine, presence: true
+  #validates :mood, presence: true
 end
