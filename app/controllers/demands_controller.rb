70720e0b307fd6ab9513cb15cc33c57bcf21a49d
hugo bosse <monsieurbosse@gmail.com>
2017-04-21 15:27:53 +0200

    cass alert and datetime

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -14,7 +14,7 @@ class DemandsController < ApplicationController
   private
 
   def demand_params
-    params.require(:demand).permit(:people)
+    params.require(:demand).permit(:people, :date)
   end
 
 end
--- app/models/demand.rb
+++ app/models/demand.rb
@@ -1,7 +1,7 @@
 class Demand < ApplicationRecord
   validates :people, presence: true
   #validates :address, presence: true
-  #validates :date, presence: true
+  validates :date, presence: true
   #validates :budget, presence: true
   #validates :phone, presence: true
   #validates :name, presence: true
