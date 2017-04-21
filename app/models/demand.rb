9fe9bfe562f7e4425c54c0ea809e899e692aaa4d
hugo bosse <monsieurbosse@gmail.com>
2017-04-21 17:38:53 +0200

    geocoder giving corect lat and lng from address

--- app/models/demand.rb
+++ app/models/demand.rb
@@ -1,6 +1,9 @@
 class Demand < ApplicationRecord
   validates :people, presence: true
   validates :address, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   validates :date, presence: true
   #validates :budget, presence: true
   #validates :phone, presence: true
