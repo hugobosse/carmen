966913c9567ea2aabc09bcb907046f3f9b7837a4
hugo bosse <monsieurbosse@gmail.com>
2017-05-22 15:53:38 +0200

    smalot datetimepicker working and rady to push

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,6 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+//= require smalot-bootstrap-datetimepicker
+//= require smalot-bootstrap-datetimepicker/locales/bootstrap-datetimepicker.fr
+
