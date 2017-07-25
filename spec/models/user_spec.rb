9670cd62ead2d0dae8f1461c3a8c3996e877c6ca
Pierre Paillere <peyop78@gmail.com>
2017-07-25 16:19:33 +0200

    created tests for mobile phone validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe User, '#mobile_phone_needed' do
   it "is not valid without a mobile phone number" do
     expect(User.new).to_not be_valid
   end
+
+  it "is not valid if mobile phone is not a french mobile phone" do
+    expect(User.new(mobile_phone: '0805060708')).to_not be_valid
+  end
+
+  it "is valid if mobile phone is a french mobile phone" do
+    expect(User.new(mobile_phone: '0705060708')).to be_valid
+  end
 end
