6697464a6bc146789bec9dca9eee24b9986a6e68
Pierre Paillere <peyop78@gmail.com>
2017-07-26 17:31:24 +0200

    add admin field to user ; configured administrate access with login

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,6 @@ Rails.application.routes.draw do
     root to: "users#index"
   end
 
-  get 'cuisines/new'
-
-  get 'cuisines/create'
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
